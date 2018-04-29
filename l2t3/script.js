80256c93857e587ec63edaa7f017c54f7e12b7dd
NatalliaHutarava <nhutarava@exadel.com>
2018-04-29 15:59:18 +0300

    homework 2 task 3 is partially done

--- l2t3/script.js
+++ l2t3/script.js
@@ -1,6 +1,7 @@
-var A = [1, 2,-3, -5, 0, -6, 10, -13, -16, 3, 3, 11, 20];
+var A = [1, 2, 5, 1, 5, -5, 0, 7, 2, 4, 1, -11, 0, 8, 8, 5, 4, 7, 9, 0, -5, -3, 0, -11];
 var B = [];
 var C = [];
+var keys;
 
 console.log ('Source array:', A);
 
@@ -13,3 +14,16 @@ C = A.filter(function(number) {
  return number < 0;
 });
 console.log ('Negative array:', C); 
+
+function unique(a) {
+  var AmReps = {};
+
+  for (var i = 0; i < A.length; i++) {
+    var str = A[i];
+    AmReps[str] = true; 
+  }
+
+  return Object.keys(AmReps); 
+}
+console.log ('unique elements', ( unique(A) )); 
+
